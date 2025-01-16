2f4f7d984dbd759c0a0f54221ba6aa13925676fc
karim ayman <karimayman050@gmail.com>
2025-01-16 23:19:22 +0200

    recommit

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -11,7 +11,7 @@ const HomePage = () => {
         const fetchPosts = async () => {
             try {
                 const response = await axios.get('http://localhost:3000/api/posts');
-                setPosts(response.data.data); // Extract the posts from the response
+                setPosts(response.data.data);
             } catch (err) {
                 setError('Failed to fetch posts. Please try again later.');
             } finally {
